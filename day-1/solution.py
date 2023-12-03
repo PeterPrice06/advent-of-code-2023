e0ee6463d950131f0c855f7bcb89cd078e0e74d2
PeterPrice06 <linebeck06@gmail.com>
2023-12-03 12:05:46 -0500

    Day 1 Part 2

--- day-1/solution.py
+++ day-1/solution.py
@@ -3,14 +3,41 @@ import re
 
 class Solution:
     numbers_pattern = re.compile(r'[0-9]')
+    spelled_digits_patterns = re.compile(r'(one|two|three|four|five|six|seven|eight|nine|[0-9])')
+    reverse_spelled_digits_patterns = re.compile(r'(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[0-9])')
 
     def __init__(self):
         pass
 
-    def solve(self, lines: List[str]) -> int:
+    def solve_part1(self, lines: List[str]) -> int:
         count = 0
         for i in range(len(lines)):
             line = lines[i]
             numbers = Solution.numbers_pattern.findall(line)
             count += int(numbers[0] + numbers[-1])
         return count
+
+    def solve_part2(self, lines: List[str]) -> int:
+        count = 0
+        for i in range(len(lines)):
+            line = lines[i]
+            first_num = Solution.spelled_digits_patterns.search(line).group(0)
+            last_num = Solution.reverse_spelled_digits_patterns.search(line[::-1]).group(0)[::-1]
+            print(f'{first_num}, {last_num}')
+            count += Solution.convert(first_num) * 10 + Solution.convert(last_num)
+        return count
+
+    def convert(s: str) -> str:
+        map = {
+            'one': 1, 
+            'two': 2, 
+            'three': 3, 
+            'four': 4, 
+            'five': 5, 
+            'six': 6, 
+            'seven': 7, 
+            'eight': 8, 
+            'nine': 9}
+        if map.get(s) != None:
+            return map.get(s)
+        return int(s)
--- day-2/test.py
+++ day-2/test.py
@@ -6,27 +6,15 @@ class TestSolution(unittest.TestCase):
     def setUp(self) -> None:
         self.solution = solution.Solution()
 
-    def test_simple_part1(self) -> None:
+    def test_simple(self) -> None:
         lines = file_read_helper('simple_input.txt')
-        result = self.solution.solve_part1(lines)
+        result = self.solution.solve(lines)
         self.assertEqual(result, 142)
 
-    def test_long_part1(self) -> None:
+    def test_long(self) -> None:
         lines = file_read_helper('long_input.txt')
-        result = self.solution.solve_part1(lines)
+        result = self.solution.solve(lines)
         self.assertEqual(result, 56108)
-
-    def test_simple_part2(self) -> None:
-        lines = file_read_helper('simple_input_part2.txt')
-        result = self.solution.solve_part2(lines)
-        self.assertEqual(result, 281)
-
-    def test_long_part2(self) -> None:
-        lines = file_read_helper('long_input.txt')
-        result = self.solution.solve_part2(lines)
-        self.assertEqual(result, '?')
-
-
 
 def file_read_helper(filename: str) -> List[str]:
     lines = []
